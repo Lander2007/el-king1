51393f82d4abbb801bf7b04420613e9161ff86b4
Mohamed Waled <waled9090mohamed@gmail.com>
2026-06-06 01:32:11 +0300

    fix: hardcode mongodb uri

--- server/src/lib/mongodb.ts
+++ server/src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://localhost:27017/wavedev_ecommerce';
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb+srv://beboeyad2008_db_user:ABHDkEFQdPuW3ac7@cluster0.deq5fv7.mongodb.net/wavedev_ecommerce?appName=Cluster0';
 
 export async function connectDB() {
   if (mongoose.connection.readyState >= 1) {
